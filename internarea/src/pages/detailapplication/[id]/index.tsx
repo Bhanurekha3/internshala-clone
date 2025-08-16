1943bb0b0775379bc51aedb856a37155f17a0bc5
Bhanurekha3 <veerlabhanurekha19@gmail.com>
2025-08-16 17:11:42 +0530

    api changes

--- internarea/src/pages/detailapplication/[id]/index.tsx
+++ internarea/src/pages/detailapplication/[id]/index.tsx
@@ -12,7 +12,7 @@ const index = () => {
         const fetchdata = async () => {
             try {
                 setloading(true);
-                const res = await axios.get(`http://localhost:5000/api/application/${id}`);
+                const res = await axios.get(`https://internshala-clone-t1w6.onrender.com/api/application/${id}`);
                 console.log(res.data);
                 setdata(res.data);
             } catch (error) {
